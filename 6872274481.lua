aafc10ae095851f1c6e672f1b02d7594a330c06b
Copium <119388242+Copiums@users.noreply.github.com>
2024-03-14 00:30:50 -0400

    Update 6872274481.lua

--- 6872274481.lua
+++ 6872274481.lua
@@ -10917,22 +10917,6 @@ stats = GuiLibrary.ObjectsThatCanBeSaved.VelocityWindow.Api.CreateOptionsButton(
     end,
     ["HoverText"] = "made by lr, velocity module"
 })
-Animatedpack = GuiLibrary.ObjectsThatCanBeSaved.RenderWindow.Api.CreateOptionsButton({
-    Name = "Animated Pack",
-    Function = function(callback)
-        workspace.CurrentCamera.Viewmodel.ChildAdded:Connect(function(x)
-            if x and x:FindFirstChild("Handle") then
-                if string.find(x.Name:lower(), 'sword') then
-                    x.Handle.Material = "ForceField"
-                    x.Handle.MeshId = "rbxassetid://13471207377"
-                    x.Handle.BrickColor = BrickColor.new("Hot pink")
-                end
-            end
-        end)
-        Animatedpack.ToggleButton(false)
-    end,
-    ["HoverText"] = "Ily Storm"
-})
 NovolithPack = GuiLibrary.ObjectsThatCanBeSaved.VelocityWindow.Api.CreateOptionsButton({
 	Name = "NovolithPack",
 	Function = function(callback)
