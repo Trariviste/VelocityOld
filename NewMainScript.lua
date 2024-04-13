cf0d18324e7eb69443eb24c3f27c494d2d89fe6d
Copium <119388242+Copiums@users.noreply.github.com>
2024-04-13 19:40:41 -0400

    Update NewMainScript.lua

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -1,4 +1,5 @@
 --Velocity unpatched
+local lplr = game:GetService("Players").LocalPlayer
 local errorPopupShown = false
 local setidentity = syn and syn.set_thread_identity or set_thread_identity or setidentity or setthreadidentity or function() end
 local getidentity = syn and syn.get_thread_identity or get_thread_identity or getidentity or getthreadidentity or function() return 8 end
